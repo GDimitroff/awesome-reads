ce40152407fd29bf78c79c048754955c962c4a04
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-08-02 13:10:46 +0300

    Basic structure of newsletter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { NewsletterComponent } from './home/newsletter/newsletter.component';
+import { ShowcaseComponent } from './home/showcase/showcase.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent,
-        HomeComponent
+        HomeComponent,
+        NewsletterComponent,
+        ShowcaseComponent
     ],
     imports: [
         BrowserModule,
