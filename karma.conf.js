cedfbd4e4db0178a170507b2ab7761e8a28f0c5f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-08-03 11:38:17 +0300

    Renaming the project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/books'),
+            dir: require('path').join(__dirname, './coverage/awesome-reads'),
             subdir: '.',
             reporters: [
                 { type: 'html' },
--- src/app/home/boxes/boxes.component.ts
+++ src/app/home/boxes/boxes.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faBook, faListAlt, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
     selector: 'app-boxes',
     templateUrl: './boxes.component.html',
     styleUrls: ['./boxes.component.css']
 })
 export class BoxesComponent implements OnInit {
+    icons = {
+        faBook,
+        faListAlt,
+        faQuoteRight
+    };
 
     constructor() { }
 
