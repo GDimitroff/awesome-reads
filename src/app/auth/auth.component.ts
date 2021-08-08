1e8c3be4a2bdc58f887780fd29528327cc192d6d
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-08-08 15:32:32 +0300

    Login request implemented

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs";
 
-import { AuthService } from "./auth.service";
+import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -27,18 +28,22 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObservable: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if (this.isLoginMode) {
-            //...
+            authObservable = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(response => {
-                console.log(response);
-                this.isLoading = false;
-            }, errorMessage => {
-                this.error = errorMessage;
-                this.isLoading = false;
-            });
+            authObservable = this.authService.signup(email, password);
         }
+
+        authObservable.subscribe(response => {
+            console.log(response);
+            this.isLoading = false;
+        }, errorMessage => {
+            this.error = errorMessage;
+            this.isLoading = false;
+        });
 
         form.reset();
     }
